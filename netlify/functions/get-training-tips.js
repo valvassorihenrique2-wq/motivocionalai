87750d73ebefcf2686bf6e1bfec443772cf580d9
henriquevalvassori8-bit <henriquevalvassori8@gmail.com>
2025-09-22 05:06:55 -0300

    aa

--- netlify/functions/get-training-tips.js
+++ netlify/functions/get-training-tips.js
@@ -5,7 +5,7 @@ const { getContent } = require('../../utils/db'); // Importa a lógica do DB
 exports.handler = async (event, context) => {
     try {
         const prompt = "Crie dicas de  treinos para inciantes em topi topicos.";
-        const data = await getContent('dicas de treino', prompt, 'tips', 'tip');
+        const data = await getContent('dicas de treino', prompt, 'tips', 'tips');
 
         return {
             statusCode: 200,
